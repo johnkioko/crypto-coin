037a2a030a08f4900975757040f4535d7586fa55
john <kiokoj81@gmail.com>
2022-12-02 20:25:37 +0300

    Add App component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Crytpos from './Components/Crytpos';
+import Crypto from './Components/Crypto';
+import NavBar from './Components/NavBar';
+import DetailsPage from './Components/DetailsPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={(
+          <>
+            <NavBar />
+            <Crytpos />
+          </>
+        )}
+      />
+      <Route
+        path="/crypto-details"
+        element={(
+          <>
+            <DetailsPage />
+            <Crypto />
+          </>
+        )}
+      />
+    </Routes>
   );
 }
 

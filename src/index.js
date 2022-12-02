ea6ea428d98c98cccd126ad16155458f14b7d061
john <kiokoj81@gmail.com>
2022-12-03 01:19:25 +0300

    Add a custome 404 error page

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import Store from './Redux/ConfigureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <Router basename={process.env.PUBLIC_URL}>
     <Provider store={Store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </Router>,
 );
